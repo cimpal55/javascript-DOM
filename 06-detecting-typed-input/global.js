296c9a024487c0ebf08c6d4331348a0826c34587
cimpal55 <cimpals71@gmail.com>
2021-03-23 14:28:57 +0200

    06 ended

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function() {
+    
+    const name = document.getElementById("full_name");
+
+    name.addEventListener("keypress", function() {
+
+        const greet = document.getElementById("greeting");
+
+        greet.innerHTML = ("Hello there, "+ name.value +"!");
+    });
+
+});
