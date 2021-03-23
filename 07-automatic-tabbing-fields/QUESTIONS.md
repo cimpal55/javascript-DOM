8f29429b99f6b8c619d5d3e72f0b984923961389
cimpal55 <cimpals71@gmail.com>
2021-03-23 14:45:17 +0200

    07 ended

--- 07-automatic-tabbing-fields/QUESTIONS.md
+++ 07-automatic-tabbing-fields/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > What are some refactoring techniques you might apply to the provided solution?
 
-Your reply here...
+Ja godīgi, es nezinu pietiekami daudz javascript, lai atbildētu uz šo jautājumu. Es izmantoju iepriekšējo nodarbību metodes. Tomēr man šķiet, ka to bija iespējams izdarīt, izmantojot const ... = function () {, un pēc tam IF un .addEventListener () tikai beigās
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,18 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function() {
+
+    const one = document.getElementById("first");
+    const two = document.getElementById("second");
+    const three = document.getElementById("third");
+
+    one.addEventListener("keypress", function() {
+        if (one.value.length === 3) {
+            two.focus();
+        }
+    two.addEventListener("keypress", function() {
+        if (two.value.length === 3) {
+            three.focus();
+        }
+    });
+});
+});
