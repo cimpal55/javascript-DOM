340a9609571c76db596037758812873bcec0a3a1
cimpal55 <cimpals71@gmail.com>
2021-03-23 14:12:55 +0200

    05 ended

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,19 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function() {
+
+    const clickt = document.getElementById("toggle_button")
+
+    clickt.addEventListener("click", function(){
+
+        const secondfiveToggle = document.querySelectorAll('ul.second_five li');
+
+            for (var i = 0; i < secondfiveToggle.length; i++) {
+                if (secondfiveToggle[i].style.display === "none") {
+                secondfiveToggle[i].style.display = "block";
+          }
+            else {
+                secondfiveToggle[i].style.display = "none";
+          }
+        }; 
+    });
+});
